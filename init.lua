571e0bc71c2bd040abb93ed4a5256dc7b986e75b
Norodix <peceli.bence@gmail.com>
2026-03-09 09:31:06 +0100

    Add diffview

--- init.lua
+++ init.lua
@@ -841,6 +841,9 @@ require('lazy').setup({
       vim.keymap.set('t', '<leader>fl', '<C-\\><C-n>:FloatermToggle<CR>', { desc = 'Toggle [Fl]oating terminal' })
     end,
   },
+  {
+    'sindrets/diffview.nvim',
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
