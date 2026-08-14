92f804c2a0ef1d7cc7791021851bc9be9ef335f5
Norodix <peceli.bence@gmail.com>
2026-08-14 21:06:11 +0200

    Enable gdscript lspconfig and activate RPC

--- init.lua
+++ init.lua
@@ -206,6 +206,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- When starting inside a godot project directory attach to the global server so godot can open scripts in nvim
+-- TODO maybe do this on every directory change?
+-- TODO maybe don't use a global pipe, maybe use them locally
+local gdpath = vim.fn.getcwd() .. '/.godot'
+if vim.uv.fs_stat(gdpath) then
+  -- Inside a godot project
+  vim.fn.serverstart '/tmp/godot.pipe'
+end
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -575,6 +584,7 @@ require('lazy').setup({
           },
         },
       }
+      vim.lsp.enable 'gdscript'
 
       -- Ensure the servers and tools above are installed
       --  To check the current status of installed tools and/or manually install
