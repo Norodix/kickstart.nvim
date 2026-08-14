02ae2f64441e65571e482ed5c5c08ab20a88104f
Norodix <peceli.bence@gmail.com>
2026-08-14 21:05:50 +0200

    Fix treesitter config for main branch

--- init.lua
+++ init.lua
@@ -841,30 +841,34 @@ require('lazy').setup({
     branch = 'main',
     build = ':TSUpdate',
     lazy = false,
-    opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'doxygen' },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    config = function()
+      require('nvim-treesitter').install {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'vim',
+        'vimdoc',
+        'doxygen',
+        'gdscript',
+      }
+      -- Tree sitter no longer enables the syntax highlights
+      -- Try to enable it for each buffer
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function(ev)
+          -- Event match is the filetype match
+          local lang = vim.treesitter.language.get_lang(ev.match)
+          -- Load the parser, if successful it can be started
+          if vim.treesitter.language.add(lang) then
+            vim.treesitter.start()
+          end
+        end,
+      })
     end,
   },
 
